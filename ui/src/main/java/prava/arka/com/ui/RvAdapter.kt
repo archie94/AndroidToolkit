bf8fed97c96df799c9899636f59f63a1888d1c5b
Arka Prava Basu <arkaprava94@gmail.com>
2018-12-25 01:35:52 +0530

    Adding UI kit
    Signed-off-by: Arka Prava Basu <arkaprava94@gmail.com>

---
+++ ui/src/main/java/prava/arka/com/ui/RvAdapter.kt
@@ -0,0 +1,8 @@
+package prava.arka.com.ui
+
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Created by Arka Prava Basu <arka.basu@zomato.com> on 17/12/18.
+ */
+abstract class RvAdapter<T : RecyclerView.ViewHolder>: RecyclerView.Adapter<T>()
