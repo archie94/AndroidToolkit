b23d1212d902eb088955bd8514433c2f260a97b6
Arka Prava Basu <arkaprava94@gmail.com>
2019-08-08 13:26:07 +0530

    Add StringUtils
    Signed-off-by: Arka Prava Basu <arkaprava94@gmail.com>

---
+++ common/src/main/java/com/prava/arka/common/StringUtils.kt
@@ -0,0 +1,16 @@
+package com.prava.arka.common
+
+import android.view.View
+
+/**
+ * Created by Arka Prava Basu<arkaprava94@gmail.com> on 2019-08-08
+ **/
+fun CharSequence?.getVisibility(hideView: Boolean = true): Int =
+    if (this == null || this.trim().isEmpty()) {
+        if (hideView)
+            View.GONE
+        else
+            View.INVISIBLE
+    } else {
+        View.INVISIBLE
+    }
