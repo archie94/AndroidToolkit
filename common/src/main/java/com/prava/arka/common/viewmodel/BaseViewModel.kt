449d4915fa76bcb423bb906bf9aaa6ea7554e573
Arka Prava Basu <arkaprava94@gmail.com>
2019-09-08 12:47:59 +0530

    Use SupervisorJob instead of Job
    Signed-off-by: Arka Prava Basu <arkaprava94@gmail.com>

--- common/src/main/java/com/prava/arka/common/viewmodel/BaseViewModel.kt
+++ common/src/main/java/com/prava/arka/common/viewmodel/BaseViewModel.kt
@@ -3,13 +3,13 @@ package com.prava.arka.common.viewmodel
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
 
 /**
  * Created by Arka Prava Basu <arkaprava94@gmail.com> on 2/9/19.
  */
 abstract class BaseViewModel : ViewModel() {
-    protected val job = Job()
+    protected val job = SupervisorJob()
     protected val uiScope = CoroutineScope(Dispatchers.Main + job)
 
     override fun onCleared() {
