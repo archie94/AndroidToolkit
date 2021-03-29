6f4bf9b28b2cbb271e08d5a5cbfaf9359009d43f
Arka Prava Basu <arkaprava94@gmail.com>
2021-03-29 20:49:46 +0530

    Add more helpers
    Signed-off-by: Arka Prava Basu <arkaprava94@gmail.com>

--- util/src/main/java/com/arka/prava/util/utils/ViewUtils.kt
+++ util/src/main/java/com/arka/prava/util/utils/ViewUtils.kt
@@ -1,8 +1,13 @@
 package com.arka.prava.util.utils
 
+import android.content.res.Resources
 import android.graphics.Rect
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 import com.arka.prava.util.core.rv.ILifecycleAwareVH
 
@@ -77,4 +82,50 @@ fun View.getRectOnScreen(): Rect {
     val l = IntArray(2)
     getLocationOnScreen(l)
     return Rect(l[0], l[1], l[0] + width, l[1] + height)
+}
+
+fun <B : ViewDataBinding> ViewGroup.inflateView(@LayoutRes layoutId: Int): View {
+    return DataBindingUtil.inflate<B>(
+        LayoutInflater.from(context),
+        layoutId,
+        this,
+        false
+    ).root
+}
+
+fun <B : ViewDataBinding> ViewGroup.inflate(@LayoutRes layoutId: Int): B {
+    return DataBindingUtil.inflate<B>(
+        LayoutInflater.from(context),
+        layoutId,
+        this,
+        false
+    )
+}
+
+fun <B : ViewDataBinding> B.setHeightInRecyclerView(viewHeight: Int) {
+    val lp: RecyclerView.LayoutParams = root.layoutParams as RecyclerView.LayoutParams
+    lp.height = viewHeight
+    root.layoutParams = lp
+}
+
+fun View.setHeight(viewHeight: Int) {
+    val lp: ViewGroup.MarginLayoutParams = layoutParams as ViewGroup.MarginLayoutParams
+    lp.height = viewHeight
+    layoutParams = lp
+}
+
+fun View.setWidth(viewWidth: Int) {
+    val lp: ViewGroup.MarginLayoutParams = layoutParams as ViewGroup.MarginLayoutParams
+    lp.width = viewWidth
+    layoutParams = lp
+}
+
+fun Resources.dp2px(dp: Float): Float {
+    val scale: Float = displayMetrics.density
+    return dp * scale + 0.5f
+}
+
+fun Resources.sp2px(sp: Float): Float {
+    val scale: Float = displayMetrics.scaledDensity
+    return sp * scale
 }
